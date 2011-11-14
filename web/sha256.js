1074c63c6b9bc6f6996c94ee7106f9a05ca2ed39
Ryan Sears <fitblip@gmail.com>
2011-11-13 22:02:12 -0500

    Changed SHA256() function to *NOT* truncate \r\n characters! :(

--- web/sha256.js
+++ web/sha256.js
@@ -5,6 +5,7 @@
 *
 *  Original code by Angel Marin, Paul Johnston.
 *
+*  Modified by Ryan Sears due to messing with \r and \n chars
 **/
  
 function SHA256(s){
@@ -85,32 +86,6 @@ function SHA256(s){
 		return bin;
 	}
  
-	function Utf8Encode(string) {
-		string = string.replace(/\r\n/g,"\n");
-		var utftext = "";
- 
-		for (var n = 0; n < string.length; n++) {
- 
-			var c = string.charCodeAt(n);
- 
-			if (c < 128) {
-				utftext += String.fromCharCode(c);
-			}
-			else if((c > 127) && (c < 2048)) {
-				utftext += String.fromCharCode((c >> 6) | 192);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
-			else {
-				utftext += String.fromCharCode((c >> 12) | 224);
-				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
- 
-		}
- 
-		return utftext;
-	}
- 
 	function binb2hex (binarray) {
 		var hex_tab = hexcase ? "0123456789ABCDEF" : "0123456789abcdef";
 		var str = "";
@@ -121,7 +96,6 @@ function SHA256(s){
 		return str;
 	}
  
-	s = Utf8Encode(s);
 	return binb2hex(core_sha256(str2binb(s), s.length * chrsz));
 
 }
